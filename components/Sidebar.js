bd96808c61dad25c0346a917a28dc13d9cbaed2a
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-17 10:39:44 +0530

    creating chat

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,21 +4,47 @@ import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchIcon from "@material-ui/icons/Search";
 import * as EmailValidator from "email-validator";
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 function Sidebar() {
+  const [user] = useAuthState(auth);
+  const userChatRef = db
+    .collection("chats")
+    .where("users", "array-contains", user.email);
+  const [chatsSnapshot] = useCollection(userChatRef);
   const createChat = () => {
     const input = prompt(
       "please enter an email address for the user you wish to chat with "
     );
     if (!input) return;
-    if (EmailValidator.validate(input)) {
+    if (
+      EmailValidator.validate(input) &&
+      !chatAlreadyExists &&
+      input !== user.email
+    ) {
+      db.collection("chats").add({
+        users: [user.email, input],
+      });
     }
   };
 
+  const chatAlreadyExists = (recipientEmail) =>
+    !!chatsSnapshot?.docs.find(
+      (chat) =>
+        chat.data().users.find((users) => user === recipientEmail)?.length > 0
+    );
   return (
     <Container>
       <Header>
-        <UserAvatar />
+        <UserAvatar
+          onClick={() => {
+            console.log("signing out");
+            auth.signOut();
+          }}
+        />
+
         <IconsContainer>
           <IconButton>
             <ChatIcon onClick={createChat} />

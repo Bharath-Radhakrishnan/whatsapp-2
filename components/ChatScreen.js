1df161d5393cd68fc9522d03f2a090b3f297f8ee
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-17 17:01:40 +0530

    chat features

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,9 +1,35 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import { auth } from "../firebase";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
 
-function ChatScreen() {
+function ChatScreen({ chat, messages }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
   return (
     <Container>
-      <h1>This is a chat</h1>
+      <Header>
+        <Avatar />
+        <HeaderInformation>
+          <h3>Rec Email</h3>
+          <p>last active ...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+      <MessageContainer>
+        {/* show messages */}
+        <EndofMessage />
+      </MessageContainer>
     </Container>
   );
 }
@@ -12,3 +38,30 @@ export default ChatScreen;
 const Container = styled.div`
   display: flex;
 `;
+const MessageContainer = styled.div`
+  /* display: flex; */
+`;
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+  > h3 {
+    margin-bottom: 3px;
+  }
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+const HeaderIcons = styled.div``;
+const EndofMessage = styled.div``;

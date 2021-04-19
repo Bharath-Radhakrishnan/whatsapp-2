664b3a4ce612826876f0079332e8bc861f15b02f
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-19 12:31:05 +0530

    add show message method

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,14 +1,46 @@
+import { useState } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
-
+import { useCollection } from "react-firebase-hooks/firestore";
+import { InsertEmoticon } from "@material-ui/icons";
+import MicIcon from "@material-ui/icons/Mic";
 function ChatScreen({ chat, messages }) {
   const [user] = useAuthState(auth);
+  const [input, setInput] = useState("");
   const router = useRouter();
+  const [messagesSnapshot] = useCollection(
+    db
+      .collection("chats")
+      .doc(router.query.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+  );
+  const showMessages = () => {
+    if (messagesSnapshot) {
+      return messagesSnapshot.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    }
+  };
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+  const sendMessage = (e) => {
+    e.preventDefault();
+  };
   return (
     <Container>
       <Header>
@@ -27,20 +59,48 @@ function ChatScreen({ chat, messages }) {
         </HeaderIcons>
       </Header>
       <MessageContainer>
-        {/* show messages */}
+        {showMessages()}
         <EndofMessage />
       </MessageContainer>
+      <InputContainer>
+        <InsertEmoticon />
+        <Input value={input} onChange={handleChange} />
+        <button hidden disabled={!input} onClick={sendMessage}>
+          Send Message
+        </button>
+        <MicIcon />
+      </InputContainer>
     </Container>
   );
 }
 
 export default ChatScreen;
-const Container = styled.div`
-  display: flex;
-`;
+const Container = styled.div``;
 const MessageContainer = styled.div`
-  /* display: flex; */
+  padding: 30px;
+  background-color: #e5ded8;
+  min-height: 90vh;
 `;
+const InputContainer = styled.form`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  position: sticky;
+  bottom: 0;
+  background-color: white;
+  z-index: 100;
+`;
+const Input = styled.div`
+  flex: 1;
+  outline: 0;
+  border: none;
+  border-radius: 10px;
+  background-color: whitesmoke;
+  padding: 20px;
+  margin-left: 15px;
+  margin-right: 15px;
+`;
+
 const Header = styled.div`
   position: sticky;
   background-color: white;
